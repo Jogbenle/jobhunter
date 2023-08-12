57ef838cbf03bcaba9c6a489f85e0cdaa4cef19b
Gbenle John Olamide <gbenlejohn2rule@gmail.com>
2023-08-12 17:07:47 +0100

    Update NavBar.jsx

--- src/Components/NavBar/NavBar.jsx
+++ src/Components/NavBar/NavBar.jsx
@@ -2,8 +2,17 @@ import React from 'react'
 
 const NavBar = () => {
   return (
-    <div>
-        
+    <div className='navBar flex justify-between items-center p-[3rem]'>
+        <div className='logoDiv'>
+            <h1 className='logo text-[25px] text-blueColor'>
+                <strong>JOB</strong>HUNTER</h1> 
+        </div>
+
+        <div className="menu flex gap-8">
+            <li className="menuList text-[#6f6f6f] hover:text-blueColor">Jobs</li>
+
+
+        </div>
     </div>
   )
 }
