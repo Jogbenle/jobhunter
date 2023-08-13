038a933334c06365faebc8d4ea138f5aa7ff1c00
Gbenle John Olamide <gbenlejohn2rule@gmail.com>
2023-08-13 14:20:32 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import React from "react";
+import NavBar from "./Components/NavBar/NavBar";
+import Search from "./Components/SearchDiv/Search";
+import Jobs from "./Components/JobDiv/jobs";
+import Value from "./Components/ValueDiv/Value";
+import Footer from "./Components/FooterDiv/Footer";
 
 const App = () => {
-  return (
-    <div>App</div>
-  )
+    return(
+        <div>
+            <NavBar/>
+            <Search/>
+            <Jobs/>
+            <Value/>
+            <Footer/>
+        </div>
+    )
 }
 
 export default App
