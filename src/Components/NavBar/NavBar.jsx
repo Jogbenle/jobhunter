b2d5238d0836be4680372190c4edead032d5860b
Gbenle John Olamide <gbenlejohn2rule@gmail.com>
2023-08-13 17:37:53 +0100

    Update NavBar.jsx

--- src/Components/NavBar/NavBar.jsx
+++ src/Components/NavBar/NavBar.jsx
@@ -10,7 +10,12 @@ const NavBar = () => {
 
         <div className="menu flex gap-8">
             <li className="menuList text-[#6f6f6f] hover:text-blueColor">Jobs</li>
-
+            <li className="menuList text-[#6f6f6f] hover:text-blueColor">Companies</li>
+            <li className="menuList text-[#6f6f6f] hover:text-blueColor">About</li>
+            <li className="menuList text-[#6f6f6f] hover:text-blueColor">Contact</li>
+            <li className="menuList text-[#6f6f6f] hover:text-blueColor">Blog</li>
+            <li className="menuList text-[#6f6f6f] hover:text-blueColor">Login</li>
+            <li className="menuList text-[#6f6f6f] hover:text-blueColor">Register</li>
 
         </div>
     </div>
