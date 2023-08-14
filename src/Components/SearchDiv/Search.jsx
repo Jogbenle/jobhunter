a45abffd77db77ab8eacd6d63991572d76a00121
Gbenle John Olamide <gbenlejohn2rule@gmail.com>
2023-08-14 19:07:48 +0100

    Update search.jsx

--- src/Components/SearchDiv/Search.jsx
+++ src/Components/SearchDiv/Search.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
-import {AiOutlineSearch} from 'react-icons/ai'
+import {AiOutlineCloseCircle, AiOutlineSearch, } from 'react-icons/ai'
+import{BsHouseDoor} from 'react-icons/bs'
+import{CiLocationOn} from 'react-icons/ci'
 
 const Search = () => {
   return (
     <div className='searchDiv grid gp-10 bg-greyIsh rounded-[10px] p-[3rem]'>
+     
       <form action="">
 
         <div className='firstDiv flex justify-between items-center rounded-[8px] 
@@ -13,10 +16,31 @@ const Search = () => {
             <AiOutlineSearch className='text-[25px] icon'/>
             <input type='text' className='bg-transparent text-blue-500 
             focus:outline-none w-[100%]' placeholder='Seach Job Here...'/>
+              <AiOutlineCloseCircle className='text-[30px] text-[#a5a6a6]
+              hover:text-textColor icon'/>
           </div>
 
+           <div className='flex gap-2 item-center'>    
+            <BsHouseDoor className='text-[25px] icon'/>
+            <input type='text' className='bg-transparent text-blue-500 
+            focus:outline-none w-[100%]' placeholder='Seach By Company...'/>
+              <AiOutlineCloseCircle className='text-[30px] text-[#a5a6a6]
+              hover:text-textColor icon'/>
+          </div>
+          
+          <div className='flex gap-2 item-center'>    
+            <CiLocationOn className='text-[25px] icon'/>
+            <input type='text' className='bg-transparent text-blue-500 
+            focus:outline-none w-[100%]' placeholder='Seach By Location...'/>
+              <AiOutlineCloseCircle className='text-[30px] text-[#a5a6a6]
+              hover:text-textColor icon'/>
+          </div>
+
+          <button className='bg-blueColor h-full p-5 px-10 rounded-[10px] text-white 
+          cursor-pointer hover:bg-blue-300'>Search</button>
         </div>
       </form>
+      
     </div>
   )
 }
