65fd55fa58c871d27e13f0c9686b2f04f4c812f9
Gbenle John Olamide <gbenlejohn2rule@gmail.com>
2023-08-16 23:57:00 +0100

    Update jobs.jsx

--- src/Components/JobDiv/Jobs.jsx
+++ src/Components/JobDiv/Jobs.jsx
@@ -2,34 +2,55 @@ import React from 'react'
 import {BiTimeFive} from 'react-icons/bi' 
 import logo1 from '../../Assets/logo (1).jpg'
 
+const Data =[
+  {
+    id:1,
+    image: logo1,
+    title: 'Web Developer',
+    time: 'Now',
+    location: 'Canada',
+    desc: 'Lorem ipsum, dolor sit amet consectetur adisoins elit. curriust loreaunsyd!',
+    company: 'Novac Linus Co.'
+  }
+]
+
+
+
 const Jobs = () => {
   return (
     <div>
       <div className="jobContainer flex gap-10 justify-center flex-wrap items-center py-10">
         
-        <div className="group group/item singleJob w-[250px] p-[20px] bg-white rounded-[10px] hover:bg-blueColor shadow-lg shadow-greyIsh-400/700 hover:shadow-lg">
+      {
+        Data.map(({id,image,title,time,location,desc,company}) =>{
+          return(
+            <div className="group group/item singleJob w-[250px] p-[20px] bg-white rounded-[10px] hover:bg-blueColor shadow-lg shadow-greyIsh-400/700 hover:shadow-lg">
 
-        <span className='flex justify-between items-center gap-4'>
-          <h1 className='text-[16px] font-semibold text-textColour group-hover:text-white'>Web Developer</h1>
-          <span className='flex items-center text-[#ccc] gap-1'>
-            <BiTimeFive/>Now
-          </span>
-        </span>
-        <h6 className='text-[#ccc]'>Canada</h6>
+              <span className='flex justify-between items-center gap-4'>
+                <h1 className='text-[16px] font-semibold text-textColour group-hover:text-white'>Web Developer</h1>
+                <span className='flex items-center text-[#ccc] gap-1'>
+                  <BiTimeFive/>Now
+                </span>
+              </span>
+              <h6 className='text-[#ccc]'>Canada</h6>
 
-        <p className='text-[13px] text-[#959595] pt-[20px] border-t-[2px] mt-[20px] group-hover:text-white'>
-          Lorem ipsum, dolor sit amet consectetur adisoins elit. curriust loreaunsyd!
-        </p>
+              <p className='text-[13px] text-[#959595] pt-[20px] border-t-[2px] mt-[20px] group-hover:text-white'>
+                Lorem ipsum, dolor sit amet consectetur adisoins elit. curriust loreaunsyd!
+              </p>
 
-        <div className='company flex items-center gap-2'>
-          <img src={logo1} alt='Company Logo' className='w-[10%]'/>
-          <span className='text-[14px] py-[1rem] block group-hover:text-white'>Novac Linus Co.</span>
-        </div>
+              <div className='company flex items-center gap-2'>
+                <img src={logo1} alt='Company Logo' className='w-[10%]'/>
+                <span className='text-[14px] py-[1rem] block group-hover:text-white'>Novac Linus Co.</span>
+              </div>
 
-        <button className='border-[2px] rounded-[10px] block p-[10px] w-full text-item:text-textColor group-hover:text-white'>
-          Apply Now
-        </button>
-        </div>
+              <button className='border-[2px] rounded-[10px] block p-[10px] w-full text-item:text-textColor group-hover:text-white'>
+                Apply Now
+              </button>
+           </div>
+          )
+        })
+      }  
+
       </div>
     </div>
   )
