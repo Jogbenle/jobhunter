9f54eff7769f4430928d34c5959bb15c00949a28
Gbenle John Olamide <gbenlejohn2rule@gmail.com>
2023-08-17 00:13:32 +0100

    Update jobs.jsx

--- src/Components/JobDiv/Jobs.jsx
+++ src/Components/JobDiv/Jobs.jsx
@@ -24,23 +24,23 @@ const Jobs = () => {
       {
         Data.map(({id,image,title,time,location,desc,company}) =>{
           return(
-            <div className="group group/item singleJob w-[250px] p-[20px] bg-white rounded-[10px] hover:bg-blueColor shadow-lg shadow-greyIsh-400/700 hover:shadow-lg">
+            <div key={id} className="group group/item singleJob w-[250px] p-[20px] bg-white rounded-[10px] hover:bg-blueColor shadow-lg shadow-greyIsh-400/700 hover:shadow-lg">
 
               <span className='flex justify-between items-center gap-4'>
-                <h1 className='text-[16px] font-semibold text-textColour group-hover:text-white'>Web Developer</h1>
+                <h1 className='text-[16px] font-semibold text-textColour group-hover:text-white'>{title}</h1>
                 <span className='flex items-center text-[#ccc] gap-1'>
-                  <BiTimeFive/>Now
+                  <BiTimeFive/>{time}
                 </span>
               </span>
-              <h6 className='text-[#ccc]'>Canada</h6>
+              <h6 className='text-[#ccc]'>{location}</h6>
 
               <p className='text-[13px] text-[#959595] pt-[20px] border-t-[2px] mt-[20px] group-hover:text-white'>
-                Lorem ipsum, dolor sit amet consectetur adisoins elit. curriust loreaunsyd!
+                {desc}
               </p>
 
               <div className='company flex items-center gap-2'>
-                <img src={logo1} alt='Company Logo' className='w-[10%]'/>
-                <span className='text-[14px] py-[1rem] block group-hover:text-white'>Novac Linus Co.</span>
+                <img src={image} alt='Company Logo' className='w-[10%]'/>
+                <span className='text-[14px] py-[1rem] block group-hover:text-white'>{company}</span>
               </div>
 
               <button className='border-[2px] rounded-[10px] block p-[10px] w-full text-item:text-textColor group-hover:text-white'>
