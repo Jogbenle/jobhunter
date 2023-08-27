70d4e0396a1206c5b1f459410375e2d049f29af3
Gbenle John Olamide <gbenlejohn2rule@gmail.com>
2023-08-27 13:57:16 +0100

    Update Value.jsx

--- src/Components/ValueDiv/Value.jsx
+++ src/Components/ValueDiv/Value.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 const Value = () => {
   return (
-    <div>Value</div>
+    <div className='mb-[4rem] mt-[6rem]'>
+     <h1 className='text-textColor text text-[25px] py-[2rem]'>The value that holds us true and to account</h1>
+    </div>
   )
 }
 
