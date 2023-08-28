3a44e1d2f23ce198288997e0b3af36daab93922a
Gbenle John Olamide <gbenlejohn2rule@gmail.com>
2023-08-28 09:46:41 +0100

    Update footer.jsx

--- src/Components/FooterDiv/Footer.jsx
+++ src/Components/FooterDiv/Footer.jsx
@@ -2,7 +2,14 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <div className='footer p-[5rem] mb-4 bg-bluecolor rounded-[10px] gap-[8] grid grid-cols-5 m-auto items-center justify-center'>
+      <div>
+      <div className='logoDiv'>
+            <h1 className='logo text-[25px] text-blueColor'>
+                <strong>JOB</strong>HUNTER</h1> 
+        </div>
+      </div>
+    </div>
   )
 }
 
